8ab51f39e7c52ba2a6230935a10e7564dcbc835f
M.K.Tonmoy <khalekuzzamantonmoy4@gmail.com>
2025-11-28 00:27:57 +0000

    auth.php

---
+++ auth.php
@@ -0,0 +1,15 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+function isLoggedIn(): bool {
+    return isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
+}
+
+function requireLogin(): void {
+    if (!isLoggedIn()) {
+        header("Location: login.php");
+        exit;
+    }
+}
